122a9c90c8d919ac80097f941c15ba6b2b17155c
Anutka Borisenko <anutkaborisenko87@gmail.com>
2023-02-25 21:07:08 +0200

    add is_admin middleware for dashboard

--- resources/views/layouts/dashboardLayout/appDashboardLayout.blade.php
+++ resources/views/layouts/dashboardLayout/appDashboardLayout.blade.php
@@ -7,16 +7,43 @@
     <title>{{ config('app.name', 'Test task full-stack position.') }}</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/dashboard/style.css') }}" rel="stylesheet">
-    <!-- Fonts -->
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-<nav>
-    <ul>
-        <li><a href="#">Ссылка 1</a></li>
-        <li><a href="#">Ссылка 2</a></li>
-        <li><a href="#">Ссылка 3</a></li>
-    </ul>
-</nav>
-@yield('content')
-</body>
+
+@guest()
+    <body style="background-color: blueviolet;">
+    @yield('content')
+    </body>
+@else
+    <body>
+    <nav>
+        <ul>
+            <li><a href="#">Ссылка 1</a></li>
+            <li><a href="#">Ссылка 2</a></li>
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </nav>
+    @yield('content')
+    </body>
+@endguest
+
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware(['auth', 'is_admin']);
